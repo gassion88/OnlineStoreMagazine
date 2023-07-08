89b1233c5c3203a035bdac08e94429d409fb537b
Soslan Gassiev <soslangassity04@gmail.com>
2023-07-08 14:24:26 +0300

    init :

---
+++ src/main/java/com/gassion/onlinestoremagazine/OnlineStoreMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.gassion.onlinestoremagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreMagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreMagazineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gassion/onlinestoremagazine/OnlineStoreMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gassion.onlinestoremagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreMagazineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
